69afe8362630e3f5a06be083007f26eda0b01263
Mike Van Bosch <falatime@gmail.com>
2013-11-07 21:07:46 -0500

    I just updated this shit u fuck

--- src/Requester.java
+++ src/Requester.java
@@ -7,115 +7,122 @@ import edu.purdue.cs.cs180.channel.ChannelException;
 import edu.purdue.cs.cs180.channel.MessageListener;
 import edu.purdue.cs.cs180.channel.TCPChannel;
 
-
 public class Requester implements MessageListener {
 
-    private Channel chan = null;
-    private Scanner scan;
-    /**
-     * Overridden Constructor.
-     * Calls method createChannel with initialized parameters.
-     */
-    public Requester(Channel channel) {
-        this.chan = channel;
-        createChannel();
-    }
-    /**
-     * Runs le Code
-     */
-    public void runner() {
-        System.out.println("1. CL50 - Class of 1950 Lecture Hall");
-        System.out.println("2. EE - Electrical Engineering Building");
-        System.out.println("3. LWSN - Lawson Computer Science Building");
-        System.out.println("4. PMU - Purdue Memorial Union");
-        System.out.println("5. PUSH - Purdue University Student Health Center");
-        String a = "";
-        System.out.print("Enter your location (1-5): ");
-        a = getInput();
-        if (a.equals("b")) {
-            System.out.println("Invalid input. Please try again.");
-            runner();
-        } else if (a.equals("f")) {
-            System.out.println("Invalid number format. Please try again.");
-            runner();
-        } else {
-            sendMessage(a);
-            System.out.println("Waiting for volunteer...");
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+	private Channel chan = null;
+	private Scanner scan;
 
-    }
-    /**
-     * gets input form user Requester
-     * @return String
-     */
-    public String getInput() {
+	/**
+	 * Overridden Constructor. Calls method createChannel with initialized
+	 * parameters.
+	 */
+	public Requester(Channel channel) {
+		this.chan = channel;
+		createChannel();
+	}
 
-        ArrayList < String > a = new ArrayList < String > ();
-        a.add("CL50");
-        a.add("EE");
-        a.add("LWSN");
-        a.add("PMU");
-        a.add("PUSH"); // adds abrev's
-        int nums = 0;
-        String s = "";
-        try {
-            s = scan.next(); //get input from user
-        } catch (InputMismatchException e) {
-            return "f";
-        }
-        try {
-            nums = Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            return "f";
-        }
+	/**
+	 * Runs le Code
+	 */
+	public void runner() {
+		System.out.println("1. CL50 - Class of 1950 Lecture Hall");
+		System.out.println("2. EE - Electrical Engineering Building");
+		System.out.println("3. LWSN - Lawson Computer Science Building");
+		System.out.println("4. PMU - Purdue Memorial Union");
+		System.out.println("5. PUSH - Purdue University Student Health Center");
+		String a = "";
+		System.out.print("Enter your location (1-5): ");
+		a = getInput();
+		if (a.equals("b")) {
+			System.out.println("Invalid input. Please try again.");
+			runner();
+		} else if (a.equals("f")) {
+			System.out.println("Invalid number format. Please try again.");
+			runner();
+		} else {
+			sendMessage(a);
+			System.out.println("Waiting for volunteer...");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
-        if (nums == 1 || nums == 2 || nums == 3 || nums == 4 || nums == 5) {
-            return ("REQUEST " + a.get(nums - 1)); // returns Request (location)
-        }
-        return "b"; // returns Request (location) unless there is an input error.
-    }
+	}
 
+	/**
+	 * gets input form user Requester
+	 * 
+	 * @return String
+	 */
+	public String getInput() {
 
-    @
-    Override
-    public void messageReceived(String message, int channelID) {
-        // simply print the message.
-        message = message.substring(message.length() - 1, message.length());
-        System.out.println("Volunteer " + message + " assigned and will arrive shortly.");
-        runner();
-    }
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("CL50");
+		a.add("EE");
+		a.add("LWSN");
+		a.add("PMU");
+		a.add("PUSH"); // adds abrev's
+		int nums = 0;
+		String s = "";
+		try {
+			s = scan.next(); // get input from user
+		} catch (InputMismatchException e) {
+			return "f";
+		}
+		try {
+			nums = Integer.parseInt(s);
+		} catch (NumberFormatException e) {
+			return "f";
+		}
 
+		if (nums == 1 || nums == 2 || nums == 3 || nums == 4 || nums == 5) {
+			return ("REQUEST " + a.get(nums - 1)); // returns Request (location)
+		}
+		return "b"; // returns Request (location) unless there is an input
+					// error.
+	}
 
-    /**
-     * Creates the TCP channel with host and port from given constructor and listens for message.
-     */
-    public void createChannel() {
-        chan.setMessageListener(this); //starts thread to listen for message back from server
-        scan = new Scanner(System. in );
-        runner();
+	@Override
+	public void messageReceived(String message, int channelID) {
+		// simply print the message.
+		message = message.substring(message.length() - 1, message.length());
+		System.out.println("Volunteer " + message
+				+ " assigned and will arrive shortly.");
+		runner();
+	}
 
-    }
-    public void sendMessage(String message) {
-        // send a message, since we did not specify a client ID, then the
-        // message will be sent to the server.
-        try {
-            chan.sendMessage(message);
-        } catch (ChannelException e) {
-            e.printStackTrace();
-        }
-    }
-    //Construct the Volunteer with input from the stream
-    public static void main(String[] args) { // args[] is the line after Java Volunteers host port
-        try {
-            new Requester(new TCPChannel("localhost", 8888));
-        } catch (ChannelException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+	/**
+	 * Creates the TCP channel with host and port from given constructor and
+	 * listens for message.
+	 */
+	public void createChannel() {
+		chan.setMessageListener(this); // starts thread to listen for message
+										// back from server
+		scan = new Scanner(System.in);
+		runner();
+
+	}
+
+	public void sendMessage(String message) {
+		// send a message, since we did not specify a client ID, then the
+		// message will be sent to the server.
+		try {
+			chan.sendMessage(message);
+		} catch (ChannelException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Construct the Volunteer with input from the stream
+	public static void main(String[] args) { // args[] is the line after Java
+												// Volunteers host port
+		try {
+			new Requester(new TCPChannel("localhost", 8888));
+		} catch (ChannelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/Server.java
+++ src/Server.java
@@ -3,103 +3,120 @@ import java.util.*;
 import edu.purdue.cs.cs180.channel.*;
 
 public class Server implements MessageListener {
-    /**
-     * Creates a server channel on the current machine and assigns port 8888 to it.
-     */
-    private Channel channel;
-    //lists for FCFS
-    private LinkedList < String > requests; //requests
-    private LinkedList < String > volunteer; //volunteers
-    private LinkedList < Integer > recID; //requests
-    private LinkedList < Integer > volID; //volunteers
-    //lists for FCFS
-    private String algorithm = "";
-    private int countVol=0;
-    /**
-     * Constructor.
-     */
-    public Server(Channel channel, String algorithm) {
-        // inform the channel that when new messages are received forward them
-        // to the current server object.
-    	this.algorithm = algorithm;
-        this.channel = channel;
-        openPort();
-    }
-    /**
-     * Opens the TCP port on the current machine for listening.
-     * initializes the linked lists.
-     * @param port
-     */
-    public void openPort() {
-    	System.out.println("Server running on port " + 8888 + " using the algorithm: " + algorithm);
-    			
-        channel.setMessageListener(this);
-        requests = new LinkedList < String > ();
-        volunteer = new LinkedList < String > ();
-        recID = new LinkedList < Integer > ();
-        volID = new LinkedList < Integer > ();
-    }
+	/**
+	 * Creates a server channel on the current machine and assigns port 8888 to
+	 * it.
+	 */
+	private Channel channel;
+	// lists for FCFS
+	private LinkedList<String> requests; // requests
+	private LinkedList<String> volunteer; // volunteers
+	private LinkedList<Integer> recID; // requests
+	private LinkedList<Integer> volID; // volunteers
+	// lists for FCFS
+	private String algorithm = "";
+	private int countVol = 0;
 
-    public void messageReceived(String message, int clientID) {
-    	System.out.println("Message receieved= " + message + " clientID= " + clientID);
-    	if (algorithm.equals("FCFS")) {
-    	FCFS(message,clientID);
-    	}
-    	else if (algorithm.equals("CLOSEST")) {
-    		
-    	}
-    	else if (algorithm.equals("URGENCY"));
-    }
-    
-    	/**
-    	 * Method to Send on Message Recieved 
-    	 * @param message
-    	 * @param clientID
-    	 * 
-    	 */
-    	public void FCFS(String message,int clientID) {
+	/**
+	 * Constructor.
+	 */
+	public Server(Channel channel, String algorithm) {
+		// inform the channel that when new messages are received forward them
+		// to the current server object.
+		this.algorithm = algorithm;
+		this.channel = channel;
+		openPort();
+	}
 
-            if (message.substring(0, 7).equals("REQUEST")) {
+	/**
+	 * Opens the TCP port on the current machine for listening. initializes the
+	 * linked lists.
+	 * 
+	 * @param port
+	 */
+	public void openPort() {
+		System.out.println("Server running on port " + 8888
+				+ " using the algorithm: " + algorithm);
 
-                if (volunteer.size() == 0) { // if there is no volunteers, add to queue
-                    recID.add(clientID); // add client id to a list
-                    requests.add(message.substring(8, message.length()));
-                } else if (volunteer.size() > 0) {
-                    try {
-                        channel.sendMessage("LOCATION " + message.substring(7, message.length()), volID.removeFirst());
-                        channel.sendMessage("VOLUNTEER " + volunteer.removeFirst(), clientID);
-                    } catch (ChannelException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } else if (message.substring(0, 9).equals("VOLUNTEER")) {
-                if (requests.size() == 0) {
-                    volID.add(clientID); //adds the client id of the volunteer to a list
-                    volunteer.add(message.substring(10, message.length()));
-                } else if (requests.size() > 0) {
-                    try {
-                        channel.sendMessage("LOCATION " + requests.removeFirst(), clientID);
-                        channel.sendMessage("VOLUNTEER " + message.substring(10, message.length()), recID.removeFirst());
-                    } catch (NumberFormatException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    } catch (ChannelException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            }
-    }
-    	public void Closest(String message, int clientID) {
-    		
-    	}
-    	
-    
-    /**
-     * constructs a new Server with the port at position args[0] ex. java Server 8888 runs with the port 8888
-     */
+		channel.setMessageListener(this);
+		requests = new LinkedList<String>();
+		volunteer = new LinkedList<String>();
+		recID = new LinkedList<Integer>();
+		volID = new LinkedList<Integer>();
+	}
 
-    public static void main(String[] args) {
-        new Server(new TCPChannel(8888),"FCFS");
-    }
+	public void messageReceived(String message, int clientID) {
+		System.out.println("Message receieved= " + message + " clientID= "
+				+ clientID);
+		if (algorithm.equals("FCFS")) {
+			FCFS(message, clientID);
+		} else if (algorithm.equals("CLOSEST")) {
+
+		} else if (algorithm.equals("URGENCY"))
+			;
+	}
+
+	/**
+	 * Method to Send on Message Recieved
+	 * 
+	 * @param message
+	 * @param clientID
+	 * 
+	 */
+	public void FCFS(String message, int clientID) {
+
+		if (message.substring(0, 7).equals("REQUEST")) {
+
+			if (volunteer.size() == 0) { // if there is no volunteers, add to
+											// queue
+				recID.add(clientID); // add client id to a list
+				requests.add(message.substring(8, message.length()));
+			} else if (volunteer.size() > 0) {
+				try {
+					channel.sendMessage(
+							"LOCATION "
+									+ message.substring(7, message.length()),
+							volID.removeFirst());
+					channel.sendMessage("VOLUNTEER " + volunteer.removeFirst(),
+							clientID);
+				} catch (ChannelException e) {
+					e.printStackTrace();
+				}
+			}
+		} else if (message.substring(0, 9).equals("VOLUNTEER")) {
+			if (requests.size() == 0) {
+				volID.add(clientID); // adds the client id of the volunteer to a
+										// list
+				volunteer.add(message.substring(10, message.length()));
+			} else if (requests.size() > 0) {
+				try {
+					channel.sendMessage("LOCATION " + requests.removeFirst(),
+							clientID);
+					channel.sendMessage(
+							"VOLUNTEER "
+									+ message.substring(10, message.length()),
+							recID.removeFirst());
+				} catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ChannelException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public void Closest(String message, int clientID) {
+
+	}
+
+	/**
+	 * constructs a new Server with the port at position args[0] ex. java Server
+	 * 8888 runs with the port 8888
+	 */
+
+	public static void main(String[] args) {
+		new Server(new TCPChannel(8888), "FCFS");
+	}
 }
